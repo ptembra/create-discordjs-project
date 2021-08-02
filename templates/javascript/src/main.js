2a5a79941b9b617bdda83a38040bdc2047f7be1c
Pedro <ph.tembra@gmail.com>
2021-08-02 18:21:11 -0300

    Minor changes and finished JS template!

---
+++ templates/javascript/src/main.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const chalk = require('chalk')
+const { config } = require('dotenv')
+
+const prefix = '!'
+
+client.on('ready', () => {
+    console.log(`%s ${client.user.username} is online!`, chalk.bgBlue.white.bold(' SUCCESS '))
+})
+
+client.on('message', async (msg) => {
+    if (msg.author.bot) return;
+    if (!msg.content.startsWith(prefix)) return
+
+    const args = msg.content.slice(prefix.length).trim().split(/ +/g)
+    const command = args.shift().toLowerCase();
+
+    if (command === 'ping') {
+        msg.channel.send(`pong :ping_pong: ${client.ws.ping}ms`);
+    }
+})
+
+config({
+    path: __dirname + "/.env",
+});
+
+client.login(process.env.TOKEN).catch(e => {
+    console.log('%s Invalid Token Provided!', chalk.bgRed.white.bold(' ERROR '))
+    console.log(e)
+})
